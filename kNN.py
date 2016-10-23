56c412e7ffe0725d41fd133b606e079c8e04b48c
Om <omjadhav@cs.wisc.edu>
2016-10-23 00:51:04 -0500

    bugs in output

--- kNN.py
+++ kNN.py
@@ -30,34 +30,13 @@ if __name__ == "__main__":
 
 	final_decision = []
 
-	# # go throuogh testing data to find out class of each instance
-	# for test_data in testDf.values:
-	# 	# test_data is a row in test data
-	# 	# print test_data
-	# 	# ecludian list
-	# 	eucl_dist = []
-		
-	# 	square_dist = 0
-
-	# 	# find ecludian distance
-	# 	for train_data in df.values:
-	# 		print train_data
-	# 		# for i, j in zip(train_data[: -1], test_data[: -1]):
-	# 		# 	square_dist = square_dist + (i - j)**2
-	# 		# eucl_dist.append(sqrt(square_dist))
-
-
-	# # print eucl_dist[0]
-
 	# go throuogh testing data to find out class of each instance
 	for test_data in testDf.values:
-			# test_data is a row in test data
-			# print test_data
-			# ecludian list
+		# test_data is a row in test data
+		
+		# ecludian list
 		eucl_dist = []
 		k_list = []
-			
-		# square_dist = 0
 
 		# find ecludian distance
 		for train_data in df.values:
@@ -73,9 +52,8 @@ if __name__ == "__main__":
 		sorted_dist_index = np.argsort(np.array(eucl_dist))
 		#print len(sorted_dist_index)
 		k_list = sorted_dist_index[:k]
-		# print eucl_dist
-		#print len(eucl_dist)
-		# print k_list
+		tempList = [eucl_dist[k_list[0]], eucl_dist[k_list[1]], eucl_dist[k_list[2]]]
+		print tempList
 
 		# assigning class/response to the current instance of test data
 		k_decisions = []
@@ -111,8 +89,29 @@ if __name__ == "__main__":
 	print final_decision
 	print len(final_decision)
 
+	# print to the console
+	correctly_classified = 0
+	print "k value : %d" %(k)
+	
+	if problemType == "classification":
 		
+		for i in range(len(final_decision)) :
+			print "Predicted class : %s\tActual class : %s" %(final_decision[i], testDf.values[i][-1])
+			if testDf.values[i][-1] == final_decision[i]:
+				correctly_classified += 1
 
+		print "Number of correctly classified instances : %d" %(correctly_classified)
+		print "Total number of instances : %d" %(len(final_decision))
+		print "Accuracy : %f" %(correctly_classified / float(len(final_decision)))
 
+	elif problemType == "regression":
+		for i in range(len(final_decision)) :
+			print "Predicted value : %s\tActual value : %s" %(final_decision[i], testDf.values[i][-1])
+			if testDf.values[i][-1] == final_decision[i]:
+				correctly_classified += 1
+			mean_error += abs(testDf.values[i][-1] - final_decision[i])
 
+		mean_error /= float(len(final_decision)) 
 
+		print "Mean absolute error : %d" %(mean_error)
+		print "Total number of instances : %d" %(len(final_decision))
